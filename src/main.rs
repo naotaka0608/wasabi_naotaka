968ff631d3450a5b4f64fb34968d26f74f0bfd55
naotaka <naotaka.levelup.project@gmail.com>
2025-06-01 05:20:37 +0900

    A6つ

--- src/main.rs
+++ src/main.rs
@@ -175,7 +175,8 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
         let _ = draw_line(&mut vram, 0xffffff, cx, cy, i, rect_size);
     }
 
-let font_a = "
+    /*
+    let font_a = "
     ........
     ...**...
     ...**...
@@ -203,7 +204,12 @@ let font_a = "
             let _ = draw_point(&mut vram, color, x as i64, y as i64);
         }
     }
- 
+    */
+    
+    for (i, c) in "ABCDEF".chars().enumerate() {
+        draw_font_fg(&mut vram, i as i64 * 16 + 256, i as i64 * 16, 0xffffff, c)
+    }
+   
 
     //println!("Hello, world!");
     //loop {}
@@ -212,6 +218,38 @@ let font_a = "
     }
 
     Ok(0)
+}
+
+fn draw_font_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, c: char) {
+    if let Ok(_c) = u8::try_from(c) {
+        let font_a = "
+........
+...**...
+...**...
+...**...
+...**...
+..*..*..
+..*..*..
+..*..*..
+..*..*..
+.******.
+.*....*.
+.*....*.
+.*....*.
+***..***
+........
+........
+";
+        for (dy, row) in font_a.trim().split('\n').enumerate() {
+            for (dx, pixel) in row.chars().enumerate() {
+                let color = match pixel {
+                    '*' => color,
+                    _ => continue,
+                };
+                let _ = draw_point(buf, color, x + dx as i64, y + dy as i64);
+            }
+        }
+    }
 }
 
 
