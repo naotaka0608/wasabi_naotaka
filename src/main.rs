257de0b5e69f2524a0eeb655a0ab2222596aebc8
naotaka <naotaka.levelup.project@gmail.com>
2025-06-06 05:32:57 +0900

    図2-18

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@
 
 use core::arch::asm;
 use core::cmp::min;
+use core::fmt;
+use core::fmt::Write;
 use core::mem::offset_of;
 use core::mem::size_of;
 use core::panic::PanicInfo;
 use core::ptr::null_mut;
+use core::writeln;
 //use core::slice;
 
 type EfiVoid = u8;
@@ -212,6 +215,11 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
    
     draw_str_fg(&mut vram, 256, 256, 0xffffff, "Hello, world!");
 
+    let mut w = VramTextWriter::new(&mut vram);
+    for i in 0..4 {
+        writeln!(w, "i = {i}").unwrap();
+    }
+   
     //println!("Hello, world!");
     //loop {}
     loop {
@@ -291,6 +299,21 @@ fn draw_str_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, s: &str) {
     for (i, c) in s.chars().enumerate() {
         draw_font_fg(buf, x + i as i64 * 8, y, color, c)
     }
+
+    struct VramTextWriter<'a> {
+        vram: &'a mut VramBufferInfo,
+    }
+    impl<'a> VramTextWriter<'a> {
+        fn new(vram: &'a mut VramBufferInfo) -> Self {
+            Self { vram }
+        }
+    }
+    impl fmt::Write for VramTextWriter<'_> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        draw_str_fg(self.vram, 0, 0, 0xffffff, s);
+        //Ok(())
+    }
+   
 }
 
 fn calc_slope_point(da: i64, db: i64, ia: i64) -> Option<i64> {
